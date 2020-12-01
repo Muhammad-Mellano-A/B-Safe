861dd01d78a0a60679db560ee16fb100c765d0dc
Muhammad-Mellano-A <18523089@students.uii.ac.id>
2020-12-01 23:18:27 +0700

    Initial commit
    UI for Login and Register's done

---
+++ app/src/main/java/com/machina/b_safe/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.machina.b_safe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginNowFromLogin(View view) {
+    }
+
+    public void registerNowFromLogin(View view) {
+    }
+}
---
+++ app/src/main/java/com/machina/b_safe/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.machina.b_safe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void onGenderChosen(View view) {
+    }
+
+    public void onAgreementClicked(View view) {
+    }
+
+    public void registerNowFromRegister(View view) {
+    }
+
+    public void loginNowFromRegister(View view) {
+    }
+}
